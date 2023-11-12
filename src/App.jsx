e990053370b316e3f37a281d392584ee0bb9d6fb
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2023-11-12 07:21:23 +0100

    descructure one react compoenet into two, and insert dynamic content there

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const randId = getRandomInt(reactDescriptions.length - 1);
+  const desc = reactDescriptions[randId];
+
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {desc} React concepts you will need for almost any app you are going to
+        build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
