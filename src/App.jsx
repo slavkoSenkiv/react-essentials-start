b8fcef64e28b029922bb136df7557ab4ce157b57
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2023-11-12 16:40:48 +0100

    discover longer and shorter props syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import reactImage from "./assets/react-core-concepts.png";
-import componentsImage from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
@@ -42,26 +41,22 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-              image={CORE_CONCEPTS[1].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[2].title}
-              description={CORE_CONCEPTS[2].description}
-              image={CORE_CONCEPTS[2].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              image={CORE_CONCEPTS[3].image}
-            />
+
+          {/* the following syntax is used when 
+          component prop names DO NOT match the object prop names */}
+          <CoreConcept
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            image={CORE_CONCEPTS[0].image}
+          />
+
+          {/* the following syntax is used when 
+          component prop names MATCH the object prop names */}
+          <CoreConcept {...CORE_CONCEPTS[1]}/>
+          <CoreConcept {...CORE_CONCEPTS[2]}/>
+          <CoreConcept {...CORE_CONCEPTS[3]}/>
+
+    
           </ul>
           <h2>Time to get started!</h2>
         </section>
