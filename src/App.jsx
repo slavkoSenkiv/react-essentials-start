36a3bb04f3432e8b0ee78e4128354a478e648f7e
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2023-11-19 07:33:20 +0100

    add dynapic tab content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import TapButton from "./components/TapButton";
 import { useState } from "react";
-
 function App() {
-  let tabContent = 'Please click a button';
-  const [ selectedTopic, setSelectedTopic ] = useState(tabContent);
+  let tabContent = "components";
+  const [selectedTopic, setSelectedTopic] = useState(tabContent);
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -20,22 +19,19 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-
-          {/* the following syntax is used when 
+            {/* the following syntax is used when 
           component prop names DO NOT match the object prop names */}
-          <CoreConcept
-            title={CORE_CONCEPTS[0].title}
-            description={CORE_CONCEPTS[0].description}
-            image={CORE_CONCEPTS[0].image}
-          />  
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
 
-          {/* the following syntax is used when 
+            {/* the following syntax is used when 
           component prop names MATCH the object prop names */}
-          <CoreConcept {...CORE_CONCEPTS[1]}/>
-          <CoreConcept {...CORE_CONCEPTS[2]}/>
-          <CoreConcept {...CORE_CONCEPTS[3]}/>
-
-    
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
           <h2>Time to get started!</h2>
         </section>
@@ -43,12 +39,19 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TapButton onSelect={() => handleSelect('components')}>Components</TapButton>
-            <TapButton onSelect={() => handleSelect('jsx')}>JSX</TapButton>
-            <TapButton onSelect={() => handleSelect('props')}>Props</TapButton>
-            <TapButton onSelect={() => handleSelect('state')}>State</TapButton>
+            <TapButton onSelect={() => handleSelect("components")}>
+              Components
+            </TapButton>
+            <TapButton onSelect={() => handleSelect("jsx")}>JSX</TapButton>
+            <TapButton onSelect={() => handleSelect("props")}>Props</TapButton>
+            <TapButton onSelect={() => handleSelect("state")}>State</TapButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content"></div>
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code}</code>
+          </pre>
         </section>
       </main>
     </div>
