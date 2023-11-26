4f3002896da16a38a302c9368702c0761f8f4090
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2023-11-26 20:19:18 +0100

    found a bug - wrote in polish letters instead of english

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -6,11 +6,11 @@ export default function CoreConcepts() {
     <section id="core-concepts">
       <h2>Core Concepts</h2>
       <ul>
-        {CORE_CONCEPTS.map((conceptItem) => {
-          <CoreConcept key={conceptItem.title} {...conceptItem} />;
-        })}
+        {CORE_CONCEPTS.map((conceptItem) => (
+          <CoreConcept key={conceptItem.title} {...conceptItem} />
+        ))}
       </ul>
-      <h2>Time to get started</h2>
+      <h2>Time to get started!</h2>
     </section>
   );
 }
